cf301502db8eed2c32441a3e8f52fb483cdea55b
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-01-30 04:30:48 +0500

    Update edit.blade.php

--- core/resources/views/backend/batchesManagement/edit.blade.php
+++ core/resources/views/backend/batchesManagement/edit.blade.php
@@ -34,7 +34,7 @@
 
             <div class="col-lg-6">
               <div class="form-group">
-                <label for="">Max Strength</label>
+                <label for="">Max Strength*</label>
                 <input type="number" id="in_maxStrength" class="form-control" name="maxStrength" placeholder="Enter Max Strength">
                 <p id="editErr_maxStrength" class="mt-2 mb-0 text-danger em"></p>
               </div>
@@ -42,9 +42,33 @@
 
             <div class="col-lg-6">
               <div class="form-group">
-                <label for="">Roll Number Prefix</label>
+                <label for="">Roll Number Prefix*</label>
                 <input type="number" id="in_rollNumberPrefix" class="form-control" name="rollNumberPrefix" placeholder="Enter Roll Number Prefix">
                 <p id="editErr_rollNumberPrefix" class="mt-2 mb-0 text-danger em"></p>
+              </div>
+            </div>
+
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label for="">Start Date*</label>
+                <input type="date" id="in_startDate" class="form-control" name="startDate" placeholder="Enter Start Date">
+                <p id="editErr_startDate" class="mt-2 mb-0 text-danger em"></p>
+              </div>
+            </div>
+
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label for="">End Date*</label>
+                <input type="date" id="in_endDate" class="form-control" name="endDate" placeholder="Enter End Date">
+                <p id="editErr_endDate" class="mt-2 mb-0 text-danger em"></p>
+              </div>
+            </div>
+
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label for="">Location*</label>
+                <input type="text" id="in_location" class="form-control" name="location" placeholder="Enter Location">
+                <p id="editErr_location" class="mt-2 mb-0 text-danger em"></p>
               </div>
             </div>
 
