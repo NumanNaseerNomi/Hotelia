52fcd786b7b2fcce4274ddef58abb7cf7747ceb3
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-01-30 01:08:05 +0500

    Update create.blade.php

--- core/resources/views/backend/coursesManagement/create.blade.php
+++ core/resources/views/backend/coursesManagement/create.blade.php
@@ -9,26 +9,26 @@
       </div>
 
       <div class="modal-body">
-        <form id="ajaxForm" class="modal-form" action="{{ url('admin/roomsManagement/create') }}" method="post">
+        <form id="ajaxForm" class="modal-form" action="{{ url('admin/coursesManagement/create') }}" method="post">
           @csrf
           <div class="row">
             <div class="col-lg-6">
               <div class="form-group">
                 <label for="">{{ __('Name') . '*' }}</label>
-                <input type="text" class="form-control" name="name" placeholder="Enter Room Name">
+                <input type="text" class="form-control" name="name" placeholder="Enter Name">
                 <p id="err_name" class="mt-2 mb-0 text-danger em"></p>
               </div>
             </div>
 
             <div class="col-lg-6">
               <div class="form-group">
-                <label for="">Type</label>
-                <input type="text" class="form-control" name="type" placeholder="Enter Type">
-                <p id="err_type" class="mt-2 mb-0 text-danger em"></p>
+                <label for="">Course Group</label>
+                <input type="text" class="form-control" name="group" placeholder="Enter Type">
+                <p id="err_group" class="mt-2 mb-0 text-danger em"></p>
               </div>
             </div>
 
-            <div class="col-lg-6">
+            <!-- <div class="col-lg-6">
               <div class="form-group">
                 <label for="">Building*</label>
                 <select class="form-control" name="buildingId">
@@ -39,9 +39,9 @@
                 </select>
                 <p id="err_buildingId" class="mt-2 mb-0 text-danger em"></p>
               </div>
-            </div>
+            </div> -->
 
-            <div class="col-lg-6">
+            <!-- <div class="col-lg-6">
               <div class="form-group">
                 <label for="">Floor*</label>
                 <select class="form-control" name="floor">
@@ -52,7 +52,7 @@
                 </select>
                 <p id="err_floor" class="mt-2 mb-0 text-danger em"></p>
               </div>
-            </div>
+            </div> -->
 
             <div class="col">
               <div class="form-group">
