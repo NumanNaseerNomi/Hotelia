fbae702b27b7386b715f2141d10fcfd790a5bc76
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-01-30 03:38:55 +0500

    Update create.blade.php

--- core/resources/views/backend/batchesManagement/create.blade.php
+++ core/resources/views/backend/batchesManagement/create.blade.php
@@ -9,13 +9,13 @@
       </div>
 
       <div class="modal-body">
-        <form id="ajaxForm" class="modal-form" action="{{ url('admin/roomsManagement/create') }}" method="post">
+        <form id="ajaxForm" class="modal-form" action="{{ url('admin/batchesManagement/create') }}" method="post">
           @csrf
           <div class="row">
             <div class="col-lg-6">
               <div class="form-group">
                 <label for="">{{ __('Name') . '*' }}</label>
-                <input type="text" class="form-control" name="name" placeholder="Enter Room Name">
+                <input type="text" class="form-control" name="name" placeholder="Enter Batch Name">
                 <p id="err_name" class="mt-2 mb-0 text-danger em"></p>
               </div>
             </div>
@@ -72,32 +72,6 @@
                 <p id="err_location" class="mt-2 mb-0 text-danger em"></p>
               </div>
             </div>
-
-            <!-- <div class="col-lg-6">
-              <div class="form-group">
-                <label for="">Building*</label>
-                <select class="form-control" name="buildingId">
-                  <option selected disabled>Select Building</option>
-                  @foreach($buildings as $building)
-                    <option value="{{ $building->id }}">{{ $building->name }}</option>
-                  @endforeach
-                </select>
-                <p id="err_buildingId" class="mt-2 mb-0 text-danger em"></p>
-              </div>
-            </div>
-
-            <div class="col-lg-6">
-              <div class="form-group">
-                <label for="">Floor*</label>
-                <select class="form-control" name="floor">
-                  <option selected disabled>Select Floor</option>
-                  @foreach(range(1, 10) as $floor)
-                    <option value="{{ $floor }}">{{ $floor }}</option>
-                  @endforeach
-                </select>
-                <p id="err_floor" class="mt-2 mb-0 text-danger em"></p>
-              </div>
-            </div> -->
 
             <div class="col">
               <div class="form-group">
