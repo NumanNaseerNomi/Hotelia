647477a36419754c594a6282ba089e84fc100465
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-01-31 02:51:34 +0500

    Update index.blade.php

--- core/resources/views/backend/studentsManagement/index.blade.php
+++ core/resources/views/backend/studentsManagement/index.blade.php
@@ -45,7 +45,7 @@
           <div class="row">
             <div class="col-lg-12">
               @if (count($batches) == 0)
-                <h3 class="text-center mt-2">NO BATCH FOUND</h3>
+                <h3 class="text-center mt-2">NO STUDENTS FOUND</h3>
               @else
                 <div class="table-responsive">
                   <table class="table table-striped mt-3" id="basic-datatables">
