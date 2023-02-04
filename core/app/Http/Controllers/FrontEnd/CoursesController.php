94c962df32c4bf6be7c4d541ae6753966dcb46ce
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-02-05 02:21:27 +0500

    Update CoursesController.php

--- core/app/Http/Controllers/FrontEnd/CoursesController.php
+++ core/app/Http/Controllers/FrontEnd/CoursesController.php
@@ -4,15 +4,7 @@ namespace App\Http\Controllers\FrontEnd;
 
 use App\Models\CoursesModel;
 use App\Traits\MiscellaneousTrait;
-use Illuminate\Support\Facades\DB;
-use App\Models\RoomManagement\Room;
 use App\Http\Controllers\Controller;
-use App\Models\RoomManagement\RoomReview;
-use App\Models\RoomManagement\RoomAmenity;
-use App\Models\RoomManagement\RoomBooking;
-use App\Models\RoomManagement\RoomContent;
-use App\Models\PaymentGateway\OnlineGateway;
-use App\Models\PaymentGateway\OfflineGateway;
 
 class CoursesController extends Controller
 {
@@ -31,7 +23,6 @@ class CoursesController extends Controller
     $data['courseDetails'] = CoursesModel::findOrFail($id);
     $data['breadcrumbInfo'] = MiscellaneousTrait::getBreadcrumb();
     $data['pageHeading'] = "Course Details";
-    // dd($data['courseDetails']->getBatches);
     return view('frontend.courseDetails', $data);
   }
 }
